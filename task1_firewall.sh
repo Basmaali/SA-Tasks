9ea42732279851eb2a506af9e072d0ca07bc8fcf
basma <root@basma>
2018-10-24 14:57:43 +0200

    Add Tasks

---
+++ task1_firewall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+iptables -A INPUT -m state --state NEW -p tcp -s 10.0.10.128 --dport 22 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp -j DROP
+iptables-save > /etc/iptables/rules.v4
