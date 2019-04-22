ba579335aa6332c481f2c90db62c8b15ce65075b
Basmaali <basma.ali.abdelsalam@gmail.com>
2019-04-22 13:09:48 +0200

    Update task1_firewall.sh

--- task1_firewall.sh
+++ task1_firewall.sh
@@ -2,3 +2,4 @@
 iptables -A INPUT -m state --state NEW -p tcp -s 10.0.10.128 --dport 22 -j ACCEPT
 iptables -A INPUT -m state --state NEW -p tcp -j DROP
 iptables-save > /etc/iptables/rules.v4
+#10.0.10.128 i my test IP
