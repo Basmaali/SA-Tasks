d07a5911d247b19bb723ea77f0bda6b26093ae1d
Basmaali <basma.ali.abdelsalam@gmail.com>
2019-05-08 00:43:26 +0200

    Update task1_shell.sh

--- task1_shell.sh
+++ task1_shell.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 echo -n Please Enter a file extension :
 read extension
-
-for f in *.jpg;
-do
-	echo -n "Please enter a file prefix (please ENTER for 2015 08 10)."
-	read prefix
-	if [ -z "$prefix" ]
-	then
-		mv "$f" "`date +%Y\ %m\ %d` $f"
-		echo "Renaming $f to "`date +%Y\ %m\ %d` $f""
-	else 
-		mv "$f" "$prefix $f"		
-		echo "Renaming $f to "$prefix $f""
-	fi
-done
+if [ -f *.$extension ]
+then
+	for f in *.$extension;
+	do
+		echo -n "Please enter a file prefix (please ENTER for 2015 08 10)."
+		read prefix
+		if [ -z "$prefix" ]
+		then
+			mv "$f" "`date +%Y\ %m\ %d` $f"
+			echo "Renaming $f to "`date +%Y\ %m\ %d` $f""
+		else 
+			mv "$f" "$prefix $f"		
+			echo "Renaming $f to "$prefix $f""
+		fi
+	done
+else
+	echo " No Such file"
+fi
